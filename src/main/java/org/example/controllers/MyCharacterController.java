2ab205948af9fd2f8b5fe9c1f00af23e45a481b0
AkagamiNoLorea <raiseta92@gmail.com>
2024-03-03 22:32:05 +0100

    fixed images

--- src/main/java/org/example/controllers/MyCharacterController.java
+++ src/main/java/org/example/controllers/MyCharacterController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @AllArgsConstructor
 public class MyCharacterController {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,16 +3,16 @@ INTO
   characters
   (ID, name, img, `description`)
 VALUES
-  (1, 'Buzz', 'buzz.img', 'Buzz Lightyear is one of the main characters in Disney/Pixar''s Toy Story franchise, voiced by Tim Allen. He is a superhero toy action figure based on the in-universe media franchise consisting of a blockbuster feature film and animated series, Space Ranger.');
+  (1, 'Buzz', 'https://www.mymims.es/wp-content/uploads/2019/12/PER_00081_PERSONAJE-BUZZ-LIGHTYEAR-TOY-STORY-H31CM.png', 'Buzz Lightyear is one of the main characters in Disney/Pixar''s Toy Story franchise, voiced by Tim Allen. He is a superhero toy action figure based on the in-universe media franchise consisting of a blockbuster feature film and animated series, Space Ranger.');
 INSERT
 INTO
   characters
   (ID, name, img, `description`)
 VALUES
-  (2, 'Woody', 'woody.img', 'Woody was designed by Bud Luckey, and is based on John Lasseter''s Casper pull-string doll he had as a kid, as well as the Howdy Doody puppets from the 1950s show. Woody is named after Woody Strode, a character actor known for many roles in western films.');
+  (2, 'Woody', 'https://www.mymims.es/wp-content/uploads/2019/12/PER_00079_PERSONAJE-WOODY-TOY-STORY-H35CM.png', 'Woody was designed by Bud Luckey, and is based on John Lasseter''s Casper pull-string doll he had as a kid, as well as the Howdy Doody puppets from the 1950s show. Woody is named after Woody Strode, a character actor known for many roles in western films.');
 INSERT
 INTO
   characters
   (ID, name, img, `description`)
 VALUES
-  (3, 'Jessie', 'jessie.img', 'Jessie is a fictional cowgirl rag doll who appears in the Disney-Pixar Toy Story franchise making her first appearance in Toy Story 2. Jessie is a brave and excitable rag doll who carries a great deal of sadness, as her original owner outgrew her and abandoned her.');
+  (3, 'Jessie', 'https://www.mymims.es/wp-content/uploads/2019/12/PER_00078_PERSONAJE-JESSIE-TOY-STORY-H34CM.png', 'Jessie is a fictional cowgirl rag doll who appears in the Disney-Pixar Toy Story franchise making her first appearance in Toy Story 2. Jessie is a brave and excitable rag doll who carries a great deal of sadness, as her original owner outgrew her and abandoned her.');
